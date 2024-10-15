f4bb7ce2b4f1e4ab074a86f817202cd595566e34
Ragde007 <edgdelcast91@gmail.com>
2024-10-15 17:04:03 -0500

    creacion estructura
    settings.py para reconocer los static, se crean las apps basicas de category, store,  accounts. se crean las carpetas de static y templates

--- xperience/settings.py
+++ xperience/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "store",
+    "category",
+    "accounts",
+    "carts",
+
 ]
 
 MIDDLEWARE = [
@@ -68,6 +73,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "xperience.wsgi.application"
+
+
+# agregar el modelo de usuarios para que se logeen
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -115,7 +124,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'xperience/static',
+]
+
+
+## configuracion de los media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
