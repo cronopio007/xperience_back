b2c6e9e85aec989fc3d3032910b0910fae9e675d
Ragde007 <edgdelcast91@gmail.com>
2024-11-05 00:01:19 -0500

    superuser y modelo de category, products
    se crea los modelos para las categorias y la de productos(store app).
    se crea fotos en el media donde se subiran las imagenes de la base

--- store/models.py
+++ store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from category.models import Category
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique = True)
+    description = models.TextField(max_length=500, blank= True)
+    price = models.IntegerField()
+    images= models.ImageField(upload_to='fotos/productos')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True) 
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_created=True)
+    modified_date = models.DateField(auto_created=True)
+
+    def get_url(self):
+        # product_detail  es el nombre que se le pone el urls.py
+        # reverse arma la url q se pasara con los argumentos
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.product_name
