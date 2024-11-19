22c09918903b3f358d735a6e0929868107e0e895
Ragde007 <edgdelcast91@gmail.com>
2024-11-19 11:23:07 -0500

    avance carrito

--- xperience/urls.py
+++ xperience/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name= 'home'),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('carts.urls')),
       #agregar los path de store
     path('store/', include('store.urls')),
 ] +     static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
